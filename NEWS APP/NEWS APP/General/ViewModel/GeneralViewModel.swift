765499ebe9edcbe5087ed4e97567cf02eabe08e4
VovaPisklov <Pisklovova@gmail.com>
2024-03-14 22:52:15 +0200

    renamed value publishedAt to date

--- NEWS APP/NEWS APP/General/ViewModel/GeneralViewModel.swift
+++ NEWS APP/NEWS APP/General/ViewModel/GeneralViewModel.swift
@@ -58,16 +58,16 @@ final class GeneralViewModel: GeneralViewModelProtocol {
     
     private func setupMockObject() {
         articles = [
-            ArticleResponseObject(title: "First object title", description: "First object description in the mock object", urlToImage: "...", publishedAt: "23.01.2012"),
-            ArticleResponseObject(title: "Second object title", description: "Second object description in the mock object", urlToImage: "...", publishedAt: "24.01.2012"),
-            ArticleResponseObject(title: "Third object title", description: "Third object description in the mock object", urlToImage: "...", publishedAt: "25.01.2012"),
-            ArticleResponseObject(title: "Fourth object title", description: "Fourth object description in the mock object", urlToImage: "...", publishedAt: "26.01.2012"),
-            ArticleResponseObject(title: "Fifth object title", description: "Fifth object description in the mock object", urlToImage: "...", publishedAt: "27.01.2012"),
-            ArticleResponseObject(title: "Sixth object title", description: "Sixth object description in the mock object", urlToImage: "...", publishedAt: "28.01.2012"),
-            ArticleResponseObject(title: "Seventh object title", description: "Seventh object description in the mock object", urlToImage: "...", publishedAt: "29.01.2012"),
-            ArticleResponseObject(title: "Eighth object title", description: "Eighth object description in the mock object", urlToImage: "...", publishedAt: "30.01.2012"),
-            ArticleResponseObject(title: "Ninth object title", description: "Ninth object description in the mock object", urlToImage: "...", publishedAt: "31.01.2012"),
-            ArticleResponseObject(title: "Tenth object title", description: "Tenth object description in the mock object", urlToImage: "...", publishedAt: "01.02.2012")
+            ArticleResponseObject(title: "First object title", description: "First object description in the mock object", urlToImage: "...", date: "23.01.2012"),
+            ArticleResponseObject(title: "Second object title", description: "Second object description in the mock object", urlToImage: "...", date: "24.01.2012"),
+            ArticleResponseObject(title: "Third object title", description: "Third object description in the mock object", urlToImage: "...", date: "25.01.2012"),
+            ArticleResponseObject(title: "Fourth object title", description: "Fourth object description in the mock object", urlToImage: "...", date: "26.01.2012"),
+            ArticleResponseObject(title: "Fifth object title", description: "Fifth object description in the mock object", urlToImage: "...", date: "27.01.2012"),
+            ArticleResponseObject(title: "Sixth object title", description: "Sixth object description in the mock object", urlToImage: "...", date: "28.01.2012"),
+            ArticleResponseObject(title: "Seventh object title", description: "Seventh object description in the mock object", urlToImage: "...", date: "29.01.2012"),
+            ArticleResponseObject(title: "Eighth object title", description: "Eighth object description in the mock object", urlToImage: "...", date: "30.01.2012"),
+            ArticleResponseObject(title: "Ninth object title", description: "Ninth object description in the mock object", urlToImage: "...", date: "31.01.2012"),
+            ArticleResponseObject(title: "Tenth object title", description: "Tenth object description in the mock object", urlToImage: "...", date: "01.02.2012")
         ]
     }
 }
