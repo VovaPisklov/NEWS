c40158cd05dfbc92a7e4d624a519b3d79a8132f7
VovaPisklov <Pisklovova@gmail.com>
2024-03-14 22:49:53 +0200

    adde ApiManager logic

--- NEWS APP/NEWS APP/Networking/ApiManager.swift
+++ NEWS APP/NEWS APP/Networking/ApiManager.swift
@@ -8,8 +8,40 @@
 import Foundation
 
 final class ApiManager {
+    private static let apiKey = "38c84392c9574e9bb4c645b8adc61f93"
+    private static let baseUrl = "https://newsapi.org/v2/"
+    private static let path = "everything"
     
-    func getNews(completion: @escaping () -> ()) {
+    // Create url path and make request
+    static func getNews(completion: @escaping (Result<[ArticleResponseObject], Error>) -> ()) {
+        let stringUrl = baseUrl + path + "?sources=bbc-news&language=en" + "&apiKey=" + apiKey
         
+        guard let url = URL(string: stringUrl) else { return }
+        
+        let session = URLSession.shared.dataTask(with: url) { data, response, error in
+            handleResponse(data: data, error: error, completion: completion)
+        }
+        
+        session.resume()
+    }
+    
+    // Handle response
+    private static func handleResponse(data: Data?,
+                                       error: Error?,
+                                       completion: @escaping (Result<[ArticleResponseObject],
+                                                              Error>) -> ()) {
+        if let error = error {
+            completion(.failure(NetworkingError.networkingError(error)))
+        } else if let data = data {
+            do {
+                let model = try JSONDecoder().decode(NewsResponseObject.self, from: data)
+                completion(.success(model.articles))
+            }
+            catch let decodeError {
+                completion(.failure(decodeError))
+            }
+        } else {
+            completion(.failure(NetworkingError.unknown))
+        }
     }
 }
