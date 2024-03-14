e33350fc1ac9f83e79e65500479add5c776649eb
VovaPisklov <Pisklovova@gmail.com>
2024-03-14 19:35:26 +0200

    added GeneralViewController code

---
+++ NEWS APP/NEWS APP/General/View/GeneralViewController.swift
@@ -0,0 +1,94 @@
+//
+//  GeneralViewController.swift
+//  NEWS APP
+//
+//  Created by Vova on 11.03.2024.
+//
+
+import UIKit
+import SnapKit
+
+class GeneralViewController: UIViewController  {
+    // MARK: - Gui Variables
+    private lazy var searchBar: UISearchBar = {
+        let serchBar = UISearchBar()
+        return serchBar
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let width = (view.frame.width - 15) / 2
+        layout.itemSize = CGSize(width: width, height: width)
+        
+        layout.minimumLineSpacing = 5
+        layout.minimumInteritemSpacing = 5
+        
+        let collectionView = UICollectionView(frame: CGRect(x: 0,
+                                                            y: 0,
+                                                            width: view.frame.width,
+                                                            height: view.frame.height - searchBar.frame.height),
+                                              collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = .white
+
+        return collectionView
+    }()
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    // MARK: - Methods
+    
+    // MARK: - Private methods
+    private func setupUI() {
+        view.backgroundColor = .white
+        view.addSubview(searchBar)
+        view.addSubview(collectionView)
+        
+        collectionView.register(GeneralCollectionViewCell.self,
+                                forCellWithReuseIdentifier: "GeneralCollectionViewCell")
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        searchBar.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom)
+            make.leading.trailing.equalToSuperview().inset(5)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension GeneralViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GeneralCollectionViewCell", for: indexPath) as? GeneralCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
+    }
+}
+
+
+// MARK: - UICollectionViewDelegate
+extension GeneralViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        navigationController?.pushViewController(NewsViewController(), animated: true)
+    }
+}
