17692418fcac011934afbd8018ddda0032162417
VovaPisklov <Pisklovova@gmail.com>
2024-03-14 21:26:44 +0200

    added implementation of ArticleCellViewModel

--- NEWS APP/NEWS APP/General/ViewModel/ArticleCellViewModel.swift
+++ NEWS APP/NEWS APP/General/ViewModel/ArticleCellViewModel.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct ArticleCellViewModel {
+    let title: String
+    let description: String
+    let data: String
+    
+    init(article: ArticleResponseObject) {
+        title = article.title
+        description = article.description
+        data = article.publishedAt
+    }
+}
