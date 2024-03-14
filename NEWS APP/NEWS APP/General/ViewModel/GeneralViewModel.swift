221e4287016492546bd10b79ef2824422ddaca3f
VovaPisklov <Pisklovova@gmail.com>
2024-03-14 19:51:56 +0200

    added GeneralViewModel

---
+++ NEWS APP/NEWS APP/General/ViewModel/GeneralViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  GeneralViewModel.swift
+//  NEWS APP
+//
+//  Created by Vova on 12.03.2024.
+//
+
+import Foundation
+
+protocol GeneralViewModelProtocol {
+    
+}
+
+final class GeneralViewModel: GeneralViewModelProtocol {
+    
+    init() {
+        loadData()
+    }
+    
+    private func loadData() {
+        // TODO: load Data
+    }
+}
