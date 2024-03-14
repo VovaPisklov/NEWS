d4335ba47409a60facecd203fa76165531188ee8
VovaPisklov <Pisklovova@gmail.com>
2024-03-14 19:34:12 +0200

    tabbar code added

---
+++ NEWS APP/NEWS APP/TabBarController.swift
@@ -0,0 +1,48 @@
+//
+//  TabBarController.swift
+//  NEWS APP
+//
+//  Created by Vova on 11.03.2024.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.tintColor = .black
+        setupViewControllers()
+        setupTabBar()
+    }
+    
+    private func setupViewControllers() {
+        
+        viewControllers = [
+            setupNavigationController(rootViewController: BusinessViewController(), title: "Bussines", image: UIImage(systemName: "briefcase") ?? UIImage.add),
+            setupNavigationController(rootViewController: TechnologyViewController(), title: "Technology", image: UIImage(systemName: "gyroscope") ?? UIImage.add),
+            
+        ]
+    }
+    
+    private func setupNavigationController(rootViewController: UIViewController,
+                                           title: String,
+                                           image: UIImage) -> UINavigationController {
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        
+        navigationController.tabBarItem.title = title
+        navigationController.tabBarItem.image = image
+        rootViewController.navigationItem.title = title
+        navigationController.navigationBar.prefersLargeTitles = true
+        
+        return navigationController
+    }
+    
+    private func setupTabBar() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        tabBar.scrollEdgeAppearance = appearance
+        
+        view.tintColor = .black
+    }
+}
