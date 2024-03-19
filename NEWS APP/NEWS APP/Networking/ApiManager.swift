e49112ed8b82f461f23466ef08615f82e13e9816
VovaPisklov <Pisklovova@gmail.com>
2024-03-19 19:55:45 +0200

    added enum Endpoint

--- NEWS APP/NEWS APP/Networking/ApiManager.swift
+++ NEWS APP/NEWS APP/Networking/ApiManager.swift
@@ -35,14 +35,20 @@ final class ApiManager {
         case technology
     }
     
+    enum Endpoint: String {
+        case everything = "everything"
+        case topHeadlines = "top-headlines"
+        case sources = "top-headlines/sources"
+    }
+    
     private static let apiKey = "38c84392c9574e9bb4c645b8adc61f93"
     private static let baseUrl = "https://newsapi.org/v2/"
-    private static let path = "everything"
+    private static let path: Endpoint = .everything
     private static let languageCode: LanguageCode = .english
 
     // Create url path and make request
     static func getNews(completion: @escaping (Result<[ArticleResponseObject], Error>) -> ()) {
-        let stringUrl = baseUrl + path + "?sources=bbc-news" + "&language=\(languageCode.rawValue)" + "&apiKey=" + apiKey
+        let stringUrl = baseUrl + path.rawValue + "?sources=bbc-news" + "&language=\(languageCode.rawValue)" + "&apiKey=" + apiKey
         
         guard let url = URL(string: stringUrl) else { return }
         
